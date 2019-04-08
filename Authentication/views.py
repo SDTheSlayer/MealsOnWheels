38ee97ef8ffd8d453200155ce4b0008448fd526f
hardy <hardikatyal@gmail.com>
2019-04-09 00:39:51 +0530

    Google auth with Django done!

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import login, logout
+
 
 # Create your views here.
+def login_page(request):
+	return render(request, 'Authentication/login_page.html')
+
+
+def home(request):
+	return render(request, 'Authentication/home.html')
+
+
+def logout_user(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('Authentication:home'))
